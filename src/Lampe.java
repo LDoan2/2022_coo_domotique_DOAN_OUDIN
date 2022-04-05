7e409736ac449a09486ac44769793357a5088b7c
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-04-05 10:44:41 +0200

    lampe

--- src/Lampe.java
+++ src/Lampe.java
@@ -1,2 +1,13 @@
 public class Lampe {
+
+    // Attributs
+    private String nom;
+    private boolean allume;
+
+    // Constructeur
+    public Lampe(String n) {
+        this.nom = n;
+        this.allume = false;
+    }
 }
+
