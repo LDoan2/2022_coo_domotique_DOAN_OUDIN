aab07610f76fda0e647ee8732fb0d1dcc6701b39
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2022-04-05 11:44:37 +0200

    tests

--- test/TelecommandeTest.java
+++ test/TelecommandeTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Test;
 import org.testng.annotations.Test;
 import static org.testng.AssertJUnit.assertEquals;
 
@@ -7,5 +8,24 @@ public class TelecommandeTest {
         Lampe l =new Lampe("Led");
         Telecommande t = new Telecommande();
         t.ajouterLampe();
+    }
+
+    @Test
+    public void activerLampe_position_1(){
+        // On prepare les donnees
+        Lampe l = new Lampe("L1");
+        Lampe l2 = new Lampe("L2");
+        Telecommande t = new Telecommande();
+        t.ajouterLampe(l);
+        t.ajouterLampe(l2);
+
+        // On execute la methode de test
+        t.activerLampe(1);
+
+        // On verifie
+        //assertEquals();
+
+
+
     }
 }
